95707dee7411c6d66ff6f5b3ab2b7c6bc567df5d
Meijix <nmejia30@ciencias.unam.mx>
2024-11-10 14:35:39 -0600

    Add ejercicio01.py

---
+++ Ejercicio-01_Numeros_pares/ejercicio01.py
@@ -0,0 +1,9 @@
+#Escribir un programa que imprima los numeros pares del 0 al 100.
+
+""" for i in range(0,101,2):
+    print(i) """
+
+numero = 2
+while numero <= 100:
+    print(numero)
+    numero += 2
