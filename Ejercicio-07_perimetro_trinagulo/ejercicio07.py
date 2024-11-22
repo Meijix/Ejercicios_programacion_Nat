cc4e4bcecdc62abf48e8f87b42ce443c874e6cf6
Meijix <nmejia30@ciencias.unam.mx>
2024-11-21 20:48:07 -0600

    Improve code

--- Ejercicio-07_perimetro_trinagulo/ejercicio07.py
+++ Ejercicio-07_perimetro_trinagulo/ejercicio07.py
@@ -2,10 +2,10 @@
 #Toma en cuenta el tipo de triangulo que es
 # Se pide al usuario que ingrese las entradas necesarias, dependiendo del tipo de triangulo
 
-#Funcion que dependiendodel numero de parametros va a calcular el perimetro de un triangulo
+#Funcion que dependiendo del numero de parametros va a calcular el perimetro de un triangulo
 #Recibe los parametros necesarios para calcular el perimetro de un triangulo
 def calcular_perimetro(opcion):
-
+    #Triangulo equilatero
     if opcion == 1:
         long_lado = float(input("Ingrese la longitud de uno de los lados del triangulo equilatero: "))
         while long_lado <= 0:
@@ -13,6 +13,7 @@ def calcular_perimetro(opcion):
             long_lado = float(input("Ingrese la longitud de uno de los lados del triangulo equilatero: "))
         perimetro = long_lado * 3
 
+    #Triangulo isosceles
     elif opcion == 2:
         base = float(input("Ingrese la longitud de la base: "))
         while base <= 0:
@@ -24,6 +25,7 @@ def calcular_perimetro(opcion):
             lado = float(input("Ingrese la longitud de los lados iguales: "))
         perimetro = base + 2 * lado
 
+    #Triangulo escaleno
     elif opcion == 3:
         lado1 = float(input("Ingrese la longitud del lado 1: "))
         while lado1 <= 0:
@@ -41,21 +43,24 @@ def calcular_perimetro(opcion):
             lado3 = float(input("Ingrese la longitud del lado 3: "))
                                 
         perimetro = lado1 + lado2 + lado3
+
+    #Opcion invalida
     else:
         print("Opcion inválida")
+        tipo_triangulo()
     return perimetro
 
 #preguntar tipo de triangulo usando una funcion
 def tipo_triangulo():
-    print("Ingresa el numero correspondiente a tu tipo de triangulo")
-    print("1. Triangulo equilatero")
-    print("2. Triangulo isosceles")
-    print("3. Triangulo escaleno")
-    opcion = int(input("Ingrese el tipo de triangulo: "))
+    print("Ingresa el número correspondiente a tu tipo de triángulo")
+    print("1. Triángulo equilátero")
+    print("2. Triángulo isoceles")
+    print("3. Triángulo escaleno")
+    opcion = int(input("Ingrese el tipo de triángulo: "))
     return opcion
 
 
 #Llama a la funcion para calcular el perimetro de un triangulo
 tipo_actual = tipo_triangulo()
 #Llama a la funcion para calcular el perimetro de un triangulo
-print("El perimetro del triangulo es: ", calcular_perimetro(tipo_actual))
+print("El perímetro del triángulo es: ", calcular_perimetro(tipo_actual))
